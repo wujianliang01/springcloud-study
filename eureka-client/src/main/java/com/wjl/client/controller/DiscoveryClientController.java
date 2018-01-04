8dd2206e3eb8b7d6f4a00eb30412290efb0f2889
WuJianLiang <498400763@qq.com>
2018-01-04 16:17:19 +0800

    2018年1月4日15:17:36

--- eureka-client/src/main/java/com/wjl/client/controller/DiscoveryClientController.java
+++ eureka-client/src/main/java/com/wjl/client/controller/DiscoveryClientController.java
@@ -12,10 +12,10 @@ public class DiscoveryClientController {
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/disconver")
-    public String getDiscoveryInfo(){
+    public String getDiscoveryInfo() throws InterruptedException{
 
         // 添加这个是为了测试(容错降级的操作， 这个其实也是可以打断点进行操作)
-        // Thread.sleep(5000L);
+        Thread.sleep(5000L);
         // 获取服务列表清单
         String service =  "Server "+discoveryClient.getServices();
 
