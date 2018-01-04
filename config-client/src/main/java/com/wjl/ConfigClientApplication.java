d03e199f4367760a7fe72d8aad19e68b61596449
WuJianLiang <498400763@qq.com>
2018-01-04 16:16:45 +0800

    2018年1月4日15:17:36

---
+++ config-client/src/main/java/com/wjl/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.wjl;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String args[]){
+
+        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+
+        System.out.println("ConfigClientApplication Start Success...");
+
+    }
+
+}
